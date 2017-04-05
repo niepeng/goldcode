4afddfcb317605db3e8cae51ec34eb345c4945bc
niepeng <niepeng>
2017-04-05 20:21:26 +0800

    add guava ratelimit opt.

---
+++ src/main/java/com/niepeng/goldcode/common/ratelimit/ApiCallDemo.java
@@ -0,0 +1,63 @@
+package com.niepeng.goldcode.common.ratelimit;
+
+import java.util.Date;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+import com.google.common.util.concurrent.RateLimiter;
+import com.niepeng.goldcode.util.DateUtil;
+
+/**
+ * 介绍文档：google的ratelimiter文档翻译
+ * http://ifeve.com/guava-ratelimiter/
+ * 
+ * @author niepeng
+ *
+ */
+public class ApiCallDemo {
+
+	private int permitsPerSecond = 10; // 每秒10个许可
+	private int threadNum = 3;
+
+	public static void main(String[] args) {
+		new ApiCallDemo().call();
+	}
+
+	private void call() {
+		ExecutorService executor = Executors.newFixedThreadPool(threadNum);
+		final RateLimiter rateLimiter = RateLimiter.create(permitsPerSecond);
+		for (int i = 0; i < threadNum; i++) {
+			executor.execute(new ApiCallTask(rateLimiter));
+		}
+		executor.shutdown();
+	}
+}
+
+class ApiCallTask implements Runnable {
+	
+	private RateLimiter rateLimiter;
+	private boolean runing = true;
+
+	public ApiCallTask(RateLimiter rateLimiter) {
+		this.rateLimiter = rateLimiter;
+	}
+
+	@Override
+	public void run() {
+		while (runing) {
+			rateLimiter.acquire(); // or rateLimiter.tryAcquire()
+			getData();
+		}
+	}
+
+	// 模拟调用合作伙伴API接口
+	private void getData() {
+		System.out.println(DateUtil.format(new Date()) + ", " +Thread.currentThread().getName() + " runing!");
+		try {
+			TimeUnit.MILLISECONDS.sleep(100);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+}
